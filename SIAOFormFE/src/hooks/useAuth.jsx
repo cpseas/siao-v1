b01800d1c7389c4dcfe8ef73d0c8b8448172b1e6
Colon0911 <calfaromedina@gmail.com>
2022-06-28 10:39:34 -0600

    io

--- SIAOFormFE/src/hooks/useAuth.jsx
+++ SIAOFormFE/src/hooks/useAuth.jsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 import { createContext, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
 import { getFullName } from "../helpers/decoding";
-=======
-import { createContext, useContext, useMemo } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useLocalStorage } from './useLocalStorage'
-import { getFullName } from '../helpers/decoding'
->>>>>>> d7597f550e96154b63675b0a6bb5d32b35b9a3aa
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-<<<<<<< HEAD
   const [token, setToken] = useLocalStorage("token", null);
   const [user, setUser] = useLocalStorage("user", null);
   const navigate = useNavigate();
@@ -29,23 +21,6 @@ export const AuthProvider = ({ children }) => {
     setUser(null);
     navigate("/", { replace: true });
   };
-=======
-	const [token, setToken] = useLocalStorage('token', null)
-	const [user, setUser] = useLocalStorage('user', null)
-	const navigate = useNavigate()
-
-	const login = async token => {
-		setToken(token)
-		setUser(getFullName(token))
-		navigate('/dashboard', { replace: true })
-	}
-
-	const logout = () => {
-		setToken(null)
-		setUser(null)
-		navigate('/', { replace: true })
-	}
->>>>>>> d7597f550e96154b63675b0a6bb5d32b35b9a3aa
 
   const value = useMemo(
     () => ({
