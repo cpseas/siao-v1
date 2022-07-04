750549e17d52e9e00246de8cf7592d384a51f86d
Colon0911 <calfaromedina@gmail.com>
2022-07-04 13:54:24 -0600

    v.1.2.3

--- SIAOFormFE/src/App.jsx
+++ SIAOFormFE/src/App.jsx
@@ -10,26 +10,31 @@ import ResetPassword from './pages/user/ResetPassword'
 
 import HomeLayouts from './layouts/HomeLayouts'
 import Profile from './pages/forms/Profile'
+import FormSolicitudRiego from './pages/forms/FormSolicitudRiego'
 
 // Primer Route = Area Publica
 function App() {
-	return (
-		<BrowserRouter>
-			<AuthProvider>
-				<Routes>
-					<Route path="/" element={<AuthLayouts />}>
-						<Route index element={<Login />} />
-						<Route path="register" element={<Register />} />
-						<Route path="forget-password" element={<ForgetPassword />} />
-						<Route path="reset-password/:token" element={<ResetPassword />} />
-					</Route>
-					<Route path="/dashboard" element={<HomeLayouts />}>
-						<Route path="profile" element={<Profile />} />
-					</Route>
-				</Routes>
-			</AuthProvider>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayouts />}>
+            <Route index element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="forget-password" element={<ForgetPassword />} />
+            <Route path="reset-password/:token" element={<ResetPassword />} />
+          </Route>
+          <Route path="/dashboard" element={<HomeLayouts />}>
+            <Route path="profile" element={<Profile />} />
+            <Route
+              path="form-solicitud-riego"
+              element={<FormSolicitudRiego />}
+            />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  )
 }
 
 export default App
